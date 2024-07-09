6a747efd6652c0ee0f71f2efa45343a4697f69c6
Aysegul-selek <aysegul.selek03@gmail.com>
2024-07-09 17:22:09 +0300

    Add project files.

---
+++ AlpataData/Entity/Meeting.cs
@@ -0,0 +1,14 @@
+﻿using AlpataTask.Core.Entities;
+
+namespace AlpataTask.Data.Entity
+{
+    public class Meeting:IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Description { get; set; }
+        public string Document { get; set; }
+    }
+}
---
+++ AlpataData/Repository/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AlpataTask.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class RecapContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Meeting> Meetings { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=AlpataDb;Trusted_Connection=true");
+        }
+       
+    }
+}
+
+
